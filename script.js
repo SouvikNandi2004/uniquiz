0f9c095648b95c4055d86c0da169dc7bae52e5c8
Souvik Nandi <83856385+SouvikNandi2004@users.noreply.github.com>
2023-04-22 15:07:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  const url = 'https://opentdb.com/api.php?amount=10&category=19&difficulty=easy&type=multiple';
+  const url = 'https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple';
 
   // Initialize variables
   let questions = [];
